521d7402e25c6b5f714f5c2201e2dbe8f5e67d35
joemarney <marney.joe@gmail.com>
2024-11-13 17:29:03 +0000

    rooms index route created and tested

--- rooms/views.py
+++ rooms/views.py
@@ -2,10 +2,15 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from utilities.exceptions import catch_exceptions
+from .serializers import RoomSerializer
 
+from .models import Room
 
 # Create your views here.
 class ListCreateRoomView(APIView):
 
+    @catch_exceptions
     def get(self, request):
-        return Response('INDEX HIT')
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data)
