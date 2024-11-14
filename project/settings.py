bb4cc5ac8d806d0e79348404b0dac769b05b63d8
joemarney <marney.joe@gmail.com>
2024-11-14 15:28:07 +0000

    cors maybe?

--- project/settings.py
+++ project/settings.py
@@ -47,11 +47,13 @@ INSTALLED_APPS = [
     'users',
     'rooms',
     'plants',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,6 +126,32 @@ AUTH_USER_MODEL = 'users.User'
 SIMPLE_JWT = { 
 	'ACCESS_TOKEN_LIFETIME': timedelta(hours=12)
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
+]
+
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
