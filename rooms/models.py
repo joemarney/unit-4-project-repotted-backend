7e81ddb369d914687292a7804aba9427c5142d7d
joemarney <marney.joe@gmail.com>
2024-11-20 12:06:43 +0000

    added image field to room model

--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField(max_length=20)
     direction_facing = models.CharField(max_length=5)
+    image = models.URLField(default="https://placehold.co/200x200")
     owner = models.ForeignKey('users.User', related_name='owned_rooms', on_delete=models.CASCADE)
 
     plants = models.ManyToManyField(
